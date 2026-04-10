3f342fce795b86f62c8cc35c1f2eb9322c09e05f
buildersbidbook <buildersbidbook@gmail.com>
2026-04-10 16:34:19 -0400

    explicitly set media_type=IMAGE for feed posts

--- src/lib/igPublish.ts
+++ src/lib/igPublish.ts
@@ -19,6 +19,7 @@ export async function createMediaContainer(
   if (isStory) {
     body.set('media_type', 'STORIES');
   } else {
+    body.set('media_type', 'IMAGE');
     body.set('caption', caption);
   }
 
